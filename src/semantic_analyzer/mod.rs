d00a58d742a5317b70312d2a3977cb39407e1653
Eunsoo Sheen <eunsoo.sheen@gmail.com>
2022-08-28 23:55:50 +0000

    Implemented pass 3

---
+++ src/semantic_analyzer/mod.rs
@@ -0,0 +1,111 @@
+mod passes;
+
+use crate::ast::Program;
+use passes::*;
+
+
+
+pub struct SemanticAnalyzer {
+    
+}
+
+impl SemanticAnalyzer {    
+    pub fn new() -> Self {
+        SemanticAnalyzer{}
+    }
+    pub fn check(&self, p: &Program) -> Result<(), Vec<SemanticCheckError>> {
+        let passes = vec![
+            has_main   
+        ];
+        let errors: Vec<SemanticCheckError> = passes.iter()
+                .map(|&pass| pass(p))
+                .filter(|res| res.is_err())
+                .map(|res| res.err().unwrap())
+                .collect();
+        if errors.len() == 0 {
+            Ok(())
+        } else {
+            Err(errors)
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests{
+    use std::fs::read_to_string;
+    use std::path::PathBuf;
+    use std::env;
+    use crate::parser::DecafParser;
+    use super::*;
+
+macro_rules! test_sa_illegal {
+    ( $testname:ident, $filename:expr ) => {
+        #[test]
+        fn $testname()
+        {
+            let path = env::var("CARGO_MANIFEST_DIR").unwrap();
+            let path: PathBuf = [&path, "src", "semantic_analyzer", "testcases", $filename].iter()
+                    .collect();
+            let s = read_to_string(&path).unwrap();
+            let program = DecafParser::new().parse(&s).unwrap();
+            let res = SemanticAnalyzer::new().check(&program);
+            assert!(res.is_err());
+            
+        }
+    };
+}
+
+macro_rules! test_sa_legal {
+    ( $testname:ident, $filename:expr ) => {
+        #[test]
+        fn $testname()
+        {
+            let path = env::var("CARGO_MANIFEST_DIR").unwrap();
+            let path: PathBuf = [&path, "src", "semantic_analyzer", "testcases", $filename].iter()
+                    .collect();
+            let s = read_to_string(&path).unwrap();
+            let program = DecafParser::new().parse(&s).unwrap();
+            let res = SemanticAnalyzer::new().check(&program);
+            assert!(res.is_ok());
+        }
+    };
+}    
+
+
+test_sa_illegal!(test_sa_illegal_01, "illegal-01.dcf");
+test_sa_illegal!(test_sa_illegal_02, "illegal-02.dcf");
+test_sa_illegal!(test_sa_illegal_03, "illegal-03.dcf");
+test_sa_illegal!(test_sa_illegal_04, "illegal-04.dcf");
+test_sa_illegal!(test_sa_illegal_05, "illegal-05.dcf");
+test_sa_illegal!(test_sa_illegal_06, "illegal-06.dcf");
+test_sa_illegal!(test_sa_illegal_07, "illegal-07.dcf");
+test_sa_illegal!(test_sa_illegal_08, "illegal-08.dcf");
+test_sa_illegal!(test_sa_illegal_09, "illegal-09.dcf");
+test_sa_illegal!(test_sa_illegal_10, "illegal-10.dcf");
+test_sa_illegal!(test_sa_illegal_11, "illegal-11.dcf");
+test_sa_illegal!(test_sa_illegal_12, "illegal-12.dcf");
+test_sa_illegal!(test_sa_illegal_13, "illegal-13.dcf");
+test_sa_illegal!(test_sa_illegal_14, "illegal-14.dcf");
+test_sa_illegal!(test_sa_illegal_15, "illegal-15.dcf");
+test_sa_illegal!(test_sa_illegal_16, "illegal-16.dcf");
+test_sa_illegal!(test_sa_illegal_17, "illegal-17.dcf");
+test_sa_legal!(test_sa_legal_01, "legal-01.dcf");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/semantic_analyzer/passes/mod.rs
@@ -0,0 +1,36 @@
+pub enum SemanticCheckError {
+    NoMainMethod,
+}
+use crate::ast::*;
+
+// 1. 	 No identifier is declared twice in the same scope. 
+// 2. 	 No identifier is used before it is declared. 
+// 3. 	 The program contains a definition for a method called main that has no parameters (note that since execution starts at method main, any methods defined after main will never be  executed). 
+pub fn has_main(p: &Program) -> Result<(),SemanticCheckError> { 
+    let has_main = p.method_decls.iter()
+                                 .filter(|&m| m.name == "main" && m.args.len() == 0)
+                                 .count() == 1;
+    if has_main {
+        Ok(())
+    } else {
+        Err(SemanticCheckError::NoMainMethod)
+    }
+}
+
+// 4. 	 The <int literal> in an array declaration must be greater than 0. 
+// 5. 	 The number and types of arguments in a method call must be the same as the number and types of the formals, i.e., the signatures must be identical. 
+// 6. 	 If a method call is used as an expression, the method must return a result. 
+// 7. 	 A return statement must not have a return value unless it appears in the body of a method that is declared to return a value. 
+// 8. 	 The expression in a return statement must have the same type as the declared result type of the enclosing method definition. 
+// 9. 	 An <id> used as a <location> must name a declared local/global variable or formal parameter. 
+// 10. For all locations of the form <id>[<expr>]
+//     (a) <id> must be an array variable, and 
+//     (b) the type of <expr> must be int. 
+// 11. The <expr> in an if statement must have type boolean. 
+// 12. The operands of <arith op>s and <rel op>s must have type int. 
+// 13. The operands of <eq op>s must have the same type, either int or boolean. 
+// 14. The operands of <cond op>s and the operand of logical not (!) must have type boolean. 
+// 15. The <location> and the <expr> in an assignment, <location> = <expr>, must have the same type. 
+// 16. The <location> and the <expr> in an incrementing/decrementing assignment, <location> += <expr> and <location> -= <expr>, must be of type int. 
+// 17. The initial <expr> and the ending <expr> of for must have type int. 
+// 18. All break and continue statements must be contained within the body of a for.
